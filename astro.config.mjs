0a50e5f4dcdb4d637f7e84114959c207da737277
Hugh Rawlinson <hughr2005@gmail.com>
2024-06-04 20:23:48 +0200

    The other

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  site: "https://hughrawlinson.github.io/",
+  base: "/hexpansion-registry/",
+});
